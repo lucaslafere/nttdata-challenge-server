5cb18c7145f1ca6e4e108b922eba230e25920098
Lucas Lafèré <laferelucas@gmail.com>
2023-03-08 17:26:35 -0300

    feat: add data filter

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,5 @@
 import { HttpService } from '@nestjs/axios/dist';
 import { Injectable, Logger } from '@nestjs/common';
-import { AxiosResponse } from 'axios';
-import { catchError, Observable } from 'rxjs';
 
 @Injectable()
 export class AppService {
@@ -13,7 +11,15 @@ export class AppService {
         `
       http://www.omdbapi.com/?i=tt3896198&apikey=613ca247&t=${title}`,
       );
-      return data;
+      // this apy key should be in a .env file, but for the purpose of the challenge, I'm leaving it here
+      let preparedData = {
+        title: data.Title,
+        actors: data.Actors,
+        poster: data.Poster,
+        plot: data.Plot,
+        rating: data.imdbRating,
+      };
+      return preparedData;
     } catch (error) {
       this.logger.error(error);
       throw error;

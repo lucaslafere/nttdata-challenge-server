6722a691e779a051fd7642597aed8688b0794870
Lucas Lafèré <laferelucas@gmail.com>
2023-03-08 17:16:12 -0300

    refactor: added try catch block

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,22 @@
 import { HttpService } from '@nestjs/axios/dist';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { AxiosResponse } from 'axios';
-import { Observable } from 'rxjs';
+import { catchError, Observable } from 'rxjs';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name);
   constructor(private httpService: HttpService) {}
   async getMovie(title: string) {
-    let { data } = await this.httpService.axiosRef.get(
-      `
+    try {
+      let { data } = await this.httpService.axiosRef.get(
+        `
       http://www.omdbapi.com/?i=tt3896198&apikey=613ca247&t=${title}`,
-    );
-    return data;
+      );
+      return data;
+    } catch (error) {
+      this.logger.error(error);
+      throw error;
+    }
   }
 }

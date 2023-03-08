9a82dd021442439fe33f9d47f6ca932135d28db8
Lucas Lafèré <laferelucas@gmail.com>
2023-03-08 17:36:53 -0300

    refactor: changed route to plural noun

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('movie/:title')
+  @Get('movies/:title')
   getMovie(@Param() params) {
     return this.appService.getMovie(params.title);
   }

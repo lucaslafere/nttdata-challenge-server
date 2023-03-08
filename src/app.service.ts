acebabe8ffd4945794cc939816630833241de9e4
Lucas Lafèré <laferelucas@gmail.com>
2023-03-08 17:10:47 -0300

    feat: add getMovie fetch function

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,10 +6,11 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class AppService {
   constructor(private httpService: HttpService) {}
-  getMovie(title: string) {
-    return this.httpService.get(
+  async getMovie(title: string) {
+    let { data } = await this.httpService.axiosRef.get(
       `
       http://www.omdbapi.com/?i=tt3896198&apikey=613ca247&t=${title}`,
     );
+    return data;
   }
 }
